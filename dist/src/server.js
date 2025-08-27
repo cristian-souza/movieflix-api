5116dfaee4277562656a7b6fb0668ca0f7316048
Cristian Souza <cristiansouza_dev@outlook.com>
2025-08-27 16:02:46 -0400

    Adicionado processo de build da aplicação

--- dist/src/server.js
+++ dist/src/server.js
@@ -2,20 +2,16 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json" with { type: "json" };
-
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
-
 app.use(express.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
             title: "asc",
         },
-
         include: {
             genres: true,
             languages: true,
@@ -23,20 +19,15 @@ app.get("/movies", async (_, res) => {
     });
     res.json(movies);
 });
-
 app.post("/movies", async (req, res) => {
-
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
-
     try {
         const movieWithSameTitle = await prisma.movie.findFirst({
             where: { title: { equals: title, mode: "insensitive" } }
         });
-
         if (movieWithSameTitle) {
-            return res.status(409).send({ message: "Já existe um filme cadastrado com esse titulo" })
+            return res.status(409).send({ message: "Já existe um filme cadastrado com esse titulo" });
         }
-
         await prisma.movie.create({
             data: {
                 title,
@@ -46,57 +37,51 @@ app.post("/movies", async (req, res) => {
                 release_date: new Date(release_date)
             }
         });
-    } catch (error) {
+    }
+    catch (error) {
         return res.status(500).send({ message: "Falha ao cadatrar um filme" });
     }
-
     res.status(201).send();
 });
-
 app.put("/movies/:id", async (req, res) => {
-    const id = Number(req.params.id)
+    const id = Number(req.params.id);
     try {
         const movie = await prisma.movie.findUnique({
             where: {
                 id
             }
         });
-
         if (!movie) {
             return res.status(404).send({ message: "Filme não encontado" });
         }
-
         const data = { ...req.body };
         data.release_date = data.release_date ? new Date(data.release_date) : undefined;
-
         await prisma.movie.update({
             where: {
                 id
             },
             data: data
         });
-    } catch (error) {
-        return res.status(500).send({ message: "Falha ao atualizar o registro do filme" })
+    }
+    catch (error) {
+        return res.status(500).send({ message: "Falha ao atualizar o registro do filme" });
     }
     res.status(200).send();
 });
-
 app.delete("/movies/:id", async (req, res) => {
     const id = Number(req.params.id);
     try {
         const movie = await prisma.movie.findUnique({ where: { id } });
-
         if (!movie) {
-            return res.status(404).send({ message: "O filme não foi encontrado" })
+            return res.status(404).send({ message: "O filme não foi encontrado" });
         }
-
         await prisma.movie.delete({ where: { id } });
-    } catch (error) {
-        return res.status(500).send({ message: "Não foi possivel remover o filme" })
+    }
+    catch (error) {
+        return res.status(500).send({ message: "Não foi possivel remover o filme" });
     }
     res.status(200).send();
 });
-
 app.get("/movies/:genreName", async (req, res) => {
     try {
         const movieFilteredByGenreName = await prisma.movie.findMany({
@@ -114,11 +99,12 @@ app.get("/movies/:genreName", async (req, res) => {
             }
         });
         res.status(200).send(movieFilteredByGenreName);
-    } catch (error) {
-        res.status(500).send({ message: "Falha ao filtar fimes por genero" })
+    }
+    catch (error) {
+        res.status(500).send({ message: "Falha ao filtar fimes por genero" });
     }
 });
-
 app.listen(port, () => {
-    console.log(`Servidor em execução na porta ${port}`)
-})
+    console.log(`Servidor em execução na porta ${port}`);
+});
+//# sourceMappingURL=server.js.map
